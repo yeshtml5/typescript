7f04796dd5c43edfa3935e6b7048d1e8e37edf16
wanhwi.son <yeshtml5@gmail.com>
2020-04-29 10:01:32 +0900

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
 /**
  * @title
  */
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 //components
 //Page
-import main from './pages/main'
-import form from './pages/form'
-import markdown from './pages/module/markdown'
+import main from "./pages/main";
+import form from "./pages/form";
+import markdown from "./pages/module/markdown";
 /*ui*/
-import tab from './pages/ui/tab'
+import tab from "./pages/ui/tab";
 //---------------------------------------------------------------------
 const App: React.FC = () => {
   //---------------------------------------------------------------------
   //initalize
   //---------------------------------------------------------------------
-  return (
-    <Switch>
-      <Route exact path="/" component={main} />
-      <Route exact path="/form" component={form} />
-      <Route exact path="/markdown" component={markdown} />
-      <Route exact path="/tab" component={tab} />
-    </Switch>
-  )
-}
+  return <h1>ddd</h1>;
+};
 
-export default App
+export default App;
