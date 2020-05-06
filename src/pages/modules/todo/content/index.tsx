54f957242598cdd27b202beb165800836811ee0d
yeshtml5 <yeshtml5@gmail.com>
2020-05-06 10:27:10 +0900

    타입스크립트 ico 추가

--- src/pages/modules/todo/content/index.tsx
+++ src/pages/modules/todo/content/index.tsx
@@ -1,1 +1,16 @@
-export {}
+import React from 'react'
+
+//type
+type Index = {
+  name: string
+  title: string
+}
+function Index({name, title}: Index) {
+  return (
+    <React.Fragment>
+      <h2>name: {name}</h2>
+      <div>title: {title}</div>
+    </React.Fragment>
+  )
+}
+export default Index
--- src/pages/modules/todo/index.tsx
+++ src/pages/modules/todo/index.tsx
@@ -1,11 +1,18 @@
+/**
+ *  @title Todo
+ *
+ */
 import React from 'react'
 //layout
 import Layout from 'pages/common/layout'
+//content
+import Index from './content/index'
 
 function Todo() {
   return (
     <Layout>
-      <h1>2</h1>
+      <h1>TodoS</h1>
+      <Index name="my_name" title="my_title" />
     </Layout>
   )
 }
