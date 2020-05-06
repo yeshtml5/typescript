eb4ed07d7921ed10fd7df918343917477328468d
yeshtml5 <yeshtml5@gmail.com>
2020-05-06 17:32:45 +0900

    context추가

--- src/pages/modules/todo/content/index.tsx
+++ src/pages/modules/todo/content/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+//context
+import {useTodosState} from '../context'
 
 //type
 type Index = {
@@ -6,10 +8,13 @@ type Index = {
   title: string
 }
 function Index({name, title}: Index) {
+  const state = useTodosState()
+  console.log(state)
   return (
     <React.Fragment>
       <h2>name: {name}</h2>
       <div>title: {title}</div>
+      <div>{JSON.stringify(state, null, 1)}</div>
     </React.Fragment>
   )
 }
--- src/pages/modules/todo/index.tsx
+++ src/pages/modules/todo/index.tsx
@@ -3,6 +3,8 @@
  *
  */
 import React from 'react'
+//context
+import {TodosContextProvider} from './context'
 //layout
 import Layout from 'pages/common/layout'
 //content
@@ -10,10 +12,12 @@ import Index from './content/index'
 
 function Todo() {
   return (
-    <Layout>
-      <h1>TodoS</h1>
-      <Index name="my_name" title="my_title" />
-    </Layout>
+    <TodosContextProvider>
+      <Layout>
+        <h1>TodoS</h1>
+        <Index name="my_name" title="my_title" />
+      </Layout>
+    </TodosContextProvider>
   )
 }
 export default Todo
