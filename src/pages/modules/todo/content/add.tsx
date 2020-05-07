dbbf7ab948a257f0493107e400f2d5de42567e59
wanhwi.son <yeshtml5@gmail.com>
2020-05-07 23:46:15 +0900

    Add Todos

--- src/pages/modules/todo/content/add.tsx
+++ src/pages/modules/todo/content/add.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 //context
 import {useTodoState, useTodoDispatch} from '../context'
@@ -7,18 +7,16 @@ function Add() {
   //context
   const state = useTodoState()
   const dispatch = useTodoDispatch()
+  //useState
+  const [value, setValue] = useState('')
   //---------------------------------------------------------------------
-  const addTodo = (): void => {
-    //dispatch({type: 'CREATE', info: {text: 'name'}})
+  const addTodo = (e: React.MouseEvent): void => {
+    dispatch({type: 'CREATE', text: value})
   }
   return (
     <Content>
-      <button
-        onClick={() => {
-          addTodo()
-        }}>
-        클릭
-      </button>
+      <input type="text" name="title" placeholder="입력해주세요" onChange={e => setValue(e.target.value)} />
+      <button onClick={addTodo}>클릭</button>
     </Content>
   )
 }
--- src/pages/modules/todo/content/index.tsx
+++ src/pages/modules/todo/content/index.tsx
@@ -10,12 +10,26 @@ type Index = {
   name: string
   title: string
 }
+
 function Index({name, title}: Index) {
   const state = useTodoState()
   console.log(state)
+
+  //makeList()
+  const makeList = () => {
+    return state.map((list, index) => {
+      return (
+        <dl>
+          <dt>{list.text}</dt>
+        </dl>
+      )
+    })
+  }
+
   return (
     <React.Fragment>
       <Add />
+      {makeList()}
       <List title="d"></List>
     </React.Fragment>
   )
